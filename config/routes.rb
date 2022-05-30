ee7366d7c4e8dfaf6eab5602fea34aea4c909661
Elizabeth Dowling <dowlinge94@gmail.com>
2022-05-30 17:45:06 -0400

    LAB COMPLETE

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
   resources :users, only: [:show]
+
+  get '/users/:user_id/items', to: 'items#index'
+  get '/users/:user_id/items/:id', to: 'items#show'
+  post '/users/:user_id/items', to: 'items#create'
 end
